ae2fa7ae7be8ad456e7039fbec6ddf95a42e4586
Suissa <jnascimento@gmail.com>
2016-11-14 19:44:31 -0200

    Subindoooooo0000ooooo

---
+++ app.js
@@ -0,0 +1,57 @@
+require('dotenv').config()
+
+const db = require('./_config/db')
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 8080
+const passport = require('passport')
+const flash = require('connect-flash')
+const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const compress = require('compression')
+const favicon = require('serve-favicon')
+
+require('./_config/passport')(passport)
+
+app.use(morgan('dev'))
+app.use(cookieParser())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(cors())
+app.use(compress()) // Compress response data with gzip
+app.use(favicon(__dirname + '/favicon.ico'))
+
+const SECRET = 'B4z1ng@#666!'
+
+// required for passport
+app.use(require('express-session')({ secret: SECRET, resave: false, saveUninitialized: false }))
+app.use(passport.initialize())
+app.use(passport.session()) //persistent login session
+app.use(flash())
+
+/* Cria as rotas dinamicamente a partir dos módulos */
+let api = {}
+let modules = require('./getModules')
+
+const createRoutes = (element, index) => {
+  api[element] = require('./modules/' + element + '/routes')
+  app.use('/api/' + element, api[element])
+}
+  /* Cria as rotas dinamicamente a partir dos módulos */
+modules.forEach(createRoutes)
+
+app.get('/ping', function (req, res, next) {
+  console.log(req.body)
+  res.send('pong')
+})
+
+app.listen(port, function () {
+  console.log('---------------------------------------------------------------------------')
+  console.log('Express server listening on port ' + port)
+  console.log('env = ' + app.get('env') +
+    '\n__dirname = ' + __dirname +
+    '\nprocess.cwd = ' + process.cwd())
+  console.log('---------------------------------------------------------------------------')
+})
---
+++ modules/users/routes.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const router = express.Router()
+const passport = require('passport')
+const Controller = require("./organism")
+
+// Create
+router.post('/', Controller.create)
+// Retrieve
+router.get('/', Controller.find)
+// get current user
+router.get('/currentuser', isLoggedIn, (req, res) => {
+  res.json(req.user)
+})
+// send JSON notifying about LOGIN
+router.get('/login', (req, res) => {
+  const loginJSON = {
+    message: "Please SEND your DATA for this ROUTE with POST"
+  }
+  res.json(loginJSON)
+})
+// logout
+router.get('/logout', (req, res) => {
+  req.logout()
+  res.status(200).send('Ok')
+})
+// get by id
+router.get('/:id', Controller.findById)
+// get one
+router.post('/find', Controller.findOne)
+// Update
+router.put('/:id', Controller.update)
+// Delete
+router.delete('/:id', Controller.remove)
+// login
+router.post('/login', passport.authenticate('local-login'), function(req, res){
+  const loginJSON = {
+    message: "User "+ req.body.email +" LOGGED!"
+  }
+  res.json(loginJSON)
+  res.end()
+})
+
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated())
+  {
+    return next()
+  }
+  res.json({success: false})
+}
+
+module.exports = router
